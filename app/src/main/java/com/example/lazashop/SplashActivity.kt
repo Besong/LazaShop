cd376c20c7efa8f27ab5566728bddb5482b7e951
besongernest <besonganong_ernest@yahoo.com>
2024-07-31 23:21:32 -0230

    splash screen implementation

---
+++ app/src/main/java/com/example/lazashop/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.example.lazashop
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+    private val delay = 2000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition{true}
+
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+
+                delay(delay)
+
+                val intent = Intent(this@SplashActivity,
+                        MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+    }
+}
+
