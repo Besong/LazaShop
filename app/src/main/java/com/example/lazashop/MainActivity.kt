ab4ceb188367d68cfd551458c919031f86351eea
besongernest <besonganong_ernest@yahoo.com>
2025-03-11 14:09:37 -0230

    Use LazaShopApp in MainActivity

--- app/src/main/java/com/example/lazashop/MainActivity.kt
+++ app/src/main/java/com/example/lazashop/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.lazashop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.lazashop.ui.screens.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WelcomeScreen()
+            LazaShopApp()
         }
     }
 }
